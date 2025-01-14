b45c2ac02047b43b23533d6250db71ce8d6286a4
Loren CHORLEY <lchorley@groupeisagri.com>
2025-01-14 16:07:21 +0100

    Simplified home page to start adding code editor

--- WebDesigner/src/app/app.component.spec.ts
+++ WebDesigner/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { signal } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
--- WebDesigner/src/app/app.component.ts
+++ WebDesigner/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Signal, signal, computed, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'WebDesigner';
+
+  public ws : WritableSignal<number> = signal(0);
+  public cs : Signal<number> = computed(() => this.ws() + 2);
 }
